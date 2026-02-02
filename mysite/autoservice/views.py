a7e0c61aa95409acf40c49e81efa8dcee2c1aa78
DonatasNoreika <donoras@gmail.com>
2026-02-02 08:12:19 +0200

    4: +Patobulinti index.html puslapį (ne admin), kuriame būtų matoma statistika: paslaugų kiekis, atliktų užsakymų kiekis, autoservise registruotų automobilių kiekis

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Service, Car, Order
 
 # Create your views here.
 def index(request):
-    return render(request, template_name="index.html")
+    context = {
+        "services": Service.objects.count(),
+        "cars": Car.objects.count(),
+        "orders_done": Order.objects.filter(status="o").count()
+    }
+    return render(request, template_name="index.html", context=context)
