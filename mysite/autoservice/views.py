b5649c5b5f99757c8d7661aede5b5107febb1f6f
DonatasNoreika <donoras@gmail.com>
2026-02-12 09:10:08 +0200

    11: +Papildomai - padaryti, kad šias eilutes galima būtų redaguoti/ištrinti.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -166,3 +166,25 @@ class OrderLineCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.Creat
         form.instance.order = Order.objects.get(pk=self.kwargs["order_pk"])
         form.save()
         return super().form_valid(form)
+
+class OrderLineUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = OrderLine
+    template_name = "orderline_form.html"
+    fields = ['service', 'quantity']
+
+    def get_success_url(self):
+        return reverse("order", kwargs={"pk": self.object.order.pk})
+
+    def test_func(self):
+        return Order.objects.get(pk=self.get_object().order.pk).client == self.request.user
+
+class OrderLineDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = OrderLine
+    template_name = "orderline_delete.html"
+    context_object_name = "orderline"
+
+    def get_success_url(self):
+        return reverse("order", kwargs={"pk": self.object.order.pk})
+
+    def test_func(self):
+        return Order.objects.get(pk=self.get_object().order.pk).client == self.request.user
